31c2b5086195804a7dfebe6538503ae4221576ba
Elizabeth Villegas <elizabeth.major@bell.ca>
2021-08-25 15:04:13 -0400

    Added example type verification to the fuzzy testing

--- atheris_test.py
+++ atheris_test.py
@@ -6,7 +6,14 @@ with atheris.instrument_imports():
     import sys
 
 
-def get_sum_then_square_root(x, y):
+def get_sum_then_square_root(x: int, y: int):
+    """
+    Performs the sum of x and y, then calculates the square root of the result
+
+    :param x: first int
+    :param y: second int
+    :return: None
+    """
     add = x + y
     print(f"{x} + {y} = {add}")
     result = sqrt(add)
@@ -15,12 +22,20 @@ def get_sum_then_square_root(x, y):
 
 @atheris.instrument_func
 def fuzzy_testing(data):
+    """
+    Performs fuzzy testing on the function get_sum_then_square_root.
+
+    :param data: data provided for fuzzy testing
+    :return: None
+    """
     sig = signature(get_sum_then_square_root)
     args = []
+    # Generate the correct amount and types of input parameters for the function
     for param in sig.parameters:
-        fdp = atheris.FuzzedDataProvider(data)
-        args.append(fdp.ConsumeInt(4))
-
+        if sig.parameters[param].annotation == int:
+            fdp = atheris.FuzzedDataProvider(data)
+            args.append(fdp.ConsumeInt(4))
+    # Run the function with the generated data
     get_sum_then_square_root(*args)
 
 
