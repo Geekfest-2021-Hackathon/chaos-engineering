6f2c2b1c0b7619ae905c1be7d18f1bedfb266f55
Elizabeth Villegas <elizabeth.major@bell.ca>
2021-08-27 09:12:40 -0400

    Added another function to test to hypothesis_test.py

--- hypothesis_test.py
+++ hypothesis_test.py
@@ -1,15 +1,26 @@
-from hypothesis import given
-from hypothesis.strategies import integers
+# Provides examples of using the Hypothesis module for fuzzy testing
+# Running the python script normally will run the tests one by one and stop if it finds an errors without performing
+# the remaining tests.
+# If you run it using pytest, then all of the tests will be run even if one fails
+
+from hypothesis import given, note, settings, infer
+from hypothesis.strategies import text
 from math import sqrt
 
 
-@given(integers(), integers())
-def test_sum_then_square_root(x, y):
+@given(x=infer, y=infer)
+def test_sum_then_square_root(x: int, y: int):
     test_sum = x + y
     if test_sum < 0:
         assert get_sum_then_square_root(x, y) is None
     else:
         assert get_sum_then_square_root(x, y) == sqrt(test_sum)
+
+
+@given(text())
+@settings(max_examples=1000)
+def test_not_kirby(s: str):
+    assert not_kirby(s)
 
 
 def get_sum_then_square_root(x: int, y: int):
@@ -22,7 +33,7 @@ def get_sum_then_square_root(x: int, y: int):
     """
     add = x + y
 
-    # --- Uncomment this block to fix the error hypothesis generates ---
+    # --- Uncomment this block to fix the error hypothesis detects ---
     # if add < 0:
     #     return None
 
@@ -30,7 +41,28 @@ def get_sum_then_square_root(x: int, y: int):
     return result
 
 
+def not_kirby(s: str):
+    note(
+        f"String received: {s}"
+    )  # This will get printed only if Hypothesis finds a problem or is running in verbose
+    if len(s) < 5:
+        return True
+
+    if s[0] == "k":
+        if s[1] == "i":
+            if s[2] == "r":
+                if s[3] == "b":
+                    if s[4] == "y":
+                        raise ValueError("Kirby is not accepted by this function.")
+
+    return True
+
+
 if __name__ == "__main__":
-    print("Running hypothesis...")
+    print("Running hypothesis for 'get_sum_then_square_root'...")
     test_sum_then_square_root()
     print("No problems found.")
+
+    print("Running hypothesis for 'not_kirby'...")
+    test_not_kirby()
+    print("No problems were found.")
